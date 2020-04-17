def7f941a71bb906cada47226fd9fcc70832acca
Edwin Anaya <eanaya@lean-tech.io>
2020-04-16 21:04:01 -0500

    feat: finish login layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Navigator} from './navigations/Navigator'
-
+import styled from 'styled-components'
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
 
       <Navigator/>
       
-    </div>
+    </Wrapper>
   );
 }
 
