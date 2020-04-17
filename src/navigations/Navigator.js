cfbe3ed0aebcb8bf5d087d1da72b8b696aba8d80
Madelin Rangel <mrangel@lean-tech.io>
2020-04-17 15:26:50 -0500

    feature/home-page: cards added

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import {NotFoundComponent} from './../components/NotFoundComponent'
-import {Login} from './../pages/Login'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NotFoundComponent } from "./../components/NotFoundComponent";
+import { Login } from "./../pages/Login";
+import Home from "./../pages/Home";
 
 function Navigator() {
-
   return (
     <Router>
       <Switch>
-        
         <Route exact path={`/`} component={Login} />
+        <Route exact path={`/home`} component={Home} />
         <Route component={NotFoundComponent} />
-
       </Switch>
     </Router>
-  )
+  );
 }
 
-export {Navigator}
+export { Navigator };
