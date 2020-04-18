637276cfe0be1b307e52fe9c4022e93aa1fd1c29
SantiagoAlvarez <salvarez@lean-tech.io>
2020-04-18 14:02:15 -0500

    feature/Movies: Detalle de pelicula

--- src/components/MovieDetail/index.jsx
+++ src/components/MovieDetail/index.jsx
@@ -1,20 +1,54 @@
 import React from 'react';
-import { Button, Heading, Text } from 'evergreen-ui';
+import { Heading, Text } from 'evergreen-ui';
 import styled from 'styled-components';
 
 function MovieDetail() {
 
-    const HedingStyle = styled(Heading)` 
-        font-size: 45px;
-        font-weight: 500;
-        line-height:60px;
-        
-        
+    const ContainerDetail = styled.div`
+        display:flex;
+        width:90%;
+        margin:  auto ;
     `
+
+    const Sypnopsys = styled.div`
+        width:40%;
+        margin-right: auto;
+        color:white;
+    `
+
+    const HedingStyle = styled(Heading)` 
+        font-size: 20px;
+        font-weight: 500;
+        line-height:60px; 
+        color:white;
+    `
+
+    const Recomendaciones = styled.div`
+        width:50%;
+        margin-right:auto;    
+    `
+
     return (
-        <div>
-            <HedingStyle>SYPNOSYS</HedingStyle>
-        </div>
+        <ContainerDetail>
+            <Sypnopsys>
+                <HedingStyle>SYPNOSYS</HedingStyle>
+                <Text color="white">Before she was Wonder Woman (Gal Gadot), she was Diana, princess of the Amazons, trained to be an unconquerable warrior.
+                Raised on a sheltered island paradise, Diana meets an American pilot (Chris Pine) who tells her about the massive conflict that's
+                raging in the outside world. Convinced that she can stop the threat, Diana leaves her home for the first time.
+                Fighting alongside men in a war to end all wars, she finally discovers her full powers and true destiny.</Text>
+            </Sypnopsys>
+            <Recomendaciones>
+                <div>
+                    <HedingStyle>DIRECTED BY</HedingStyle>
+                    <Text color="white">PATTY JENKINS</Text>
+                </div>
+                <div>
+                    <HedingStyle>RECOMMENDEN BY</HedingStyle>
+                    <Text color="white">RIO AND JAMES </Text>
+                </div>
+            </Recomendaciones>
+
+        </ContainerDetail>
     )
 }
 
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { Header } from './../../components/Header';
+import { MovieDetail } from './../../components/MovieDetail';
+import { ContainerTitle } from '../../components/ContainerTitle';
+import backgroundDetails from './../../assets/images/backgroundDetails.jpg';
+import styled from 'styled-components';
 
 function Details() {
+
+    const ContenedorDetail = styled.div`
+        background-image: url(${backgroundDetails});
+        background-repeat: no-repeat;
+	    background-size: cover;
+        width: 100%;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+    `
     return (
-        <Header />
+        <ContenedorDetail>
+            <Header />
+            <ContainerTitle />
+            <MovieDetail />
+        </ContenedorDetail>
+
     )
 }
 
