14a160371f8ad5e01f6cd5de18d63497a073963b
Madelin Rangel <mrangel@lean-tech.io>
2020-04-19 21:55:30 -0500

    Constans created

---
+++ src/utils/Constans.js
@@ -0,0 +1,8 @@
+export default {
+  loginAction: "LOGIN_ACTION",
+  SuccessFetchLogin: "SUCCESS_FETCH_LOGIN",
+  FailFetchLogin: "FAIL_FETCH_LOGIN",
+  trendingAction: "TRENDING_ACTION",
+  SuccessFetchTrending: "SUCCESS_FETCH_TRENDING",
+  FailFetchTrending: "FAIL_FETCH_TRENDING",
+};
