b96aa295dea6d32eb80b6108c77980ae57b5ff45
Madelin Rangel <mrangel@lean-tech.io>
2020-04-19 22:10:23 -0500

    Use sagas on Login page

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NotFoundComponent } from "./../components/NotFoundComponent";
-import { Login } from "./../pages/Login";
+import Login from "./../pages/Login";
 import Home from "./../pages/Home";
 import Details from "./../pages/Details";
 
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,70 +1,94 @@
-import React,{useState} from 'react'
-import styled from 'styled-components'
-import { Pane, TextInput, Button, Heading } from 'evergreen-ui'
-import background from './../../assets/images/background.jpg';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Pane, TextInput, Button, Heading } from "evergreen-ui";
+import background from "./../../assets/images/background.jpg";
+import { connect } from "react-redux";
+import { LoginAction } from "./../../redux/actions/login.actions";
 
 const Wrapper = styled.div`
-	display: flex;
-	width: 100%;
-	height: 100%;
-	justify-content: center;
-	align-items: center;
-	background-image: url(${background});
-	background-repeat: no-repeat;
-	background-size: cover;
-`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
 const FormContainer = styled(Pane)`
-	width: 300px;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 const Title = styled(Heading)`
-	margin-bottom: 50px;
-	color: white;
-`
+  margin-bottom: 50px;
+  color: white;
+`;
 const Input = styled(TextInput)`
-	width: 100%;
-	margin-bottom: 10px;
-	height: 40px;
-`
+  width: 100%;
+  margin-bottom: 10px;
+  height: 40px;
+`;
 const Btn = styled(Button)`
-	width: 100%;
-	margin-bottom: 20px;
-	height: 40px;
-	display: flex;
-	justify-content: center;
-`
+  width: 100%;
+  margin-bottom: 20px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+`;
 
-function Login (){
+function Login({ LOGIN_RESPONSE, LoginAction }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+  useEffect(() => {
+    if (!LOGIN_RESPONSE) return;
 
-	const FormElement= (
-		<FormContainer>
-			<Title size={800} marginTop="default">MOVIES</Title>
-			<Input
-				name="text-input-email"
-				placeholder="Enter your Email"
-				value={email}
-				onChange={e => setEmail(e.target.value)}
-			/>
-			<Input
-				name="text-input-password"
-				placeholder="Enter your Password"
-				value={password}
-				onChange={e => setPassword(e.target.value)}
-			/>
-			<Btn appearance="primary" onClick={() => { console.log(`Email: ${email}, Password: ${password}`) }}>Sign in</Btn>
-		</FormContainer>
-	)
+    if (LOGIN_RESPONSE.status !== 200) {
+      //handle error
+      return;
+    }
 
-	return(
-		<Wrapper>
-			{FormElement}
-		</Wrapper>
-	)
+    //handle success
+    console.log(`Welcome ${LOGIN_RESPONSE.res.displayName}`);
+  }, [LOGIN_RESPONSE]);
+
+  const FormElement = (
+    <FormContainer>
+      <Title size={800} marginTop="default">
+        MOVIES
+      </Title>
+      <Input
+        name="text-input-email"
+        placeholder="Enter your Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        name="text-input-password"
+        placeholder="Enter your Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Btn
+        appearance="primary"
+        onClick={() => {
+          LoginAction({ email, password });
+        }}
+      >
+        Sign in
+      </Btn>
+    </FormContainer>
+  );
+
+  return <Wrapper>{FormElement}</Wrapper>;
 }
 
-export {Login}
+const mapStateToProps = (state, props) => {
+  return {
+    LOGIN_RESPONSE: state.loginReducers.LoginResponse,
+  };
+};
+
+export default connect(mapStateToProps, { LoginAction })(Login);
