7788c9ea59433147b6c702c85969442e5558b6b3
Madelin Rangel <mrangel@lean-tech.io>
2020-04-24 20:16:17 -0500

    New pages created

---
+++ src/pages/Channel/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyledHeading, Navbar } from "../../components";
+
+function Channel() {
+  return (
+    <>
+      <Navbar />
+      <StyledHeading
+        textAlign="center"
+        fontSize="30px"
+        lineHeight="300px"
+        textTransform="capitalize"
+      >
+        Page under construction
+      </StyledHeading>
+    </>
+  );
+}
+
+export default Channel;
---
+++ src/pages/Settings/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyledHeading, Navbar } from "../../components";
+
+function Settings() {
+  return (
+    <>
+      <Navbar />
+      <StyledHeading
+        textAlign="center"
+        fontSize="30px"
+        lineHeight="300px"
+        textTransform="capitalize"
+      >
+        Page under construction
+      </StyledHeading>
+    </>
+  );
+}
+
+export default Settings;
